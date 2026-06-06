6cb7c76a5c49fc97339bfa381ba9d23b783c1d51
Rushikesh <157354662+R-Dhawale@users.noreply.github.com>
2026-06-06 22:06:32 +0530

    implement navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Header from "@/components/Header";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
@@ -28,9 +29,12 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full dark", "antialiased", geistSans.variable, geistMono.variable, "font-sans", geist.variable)}
+      className="dark"
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className= {`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />  
+        {children}
+      </body>
     </html>
   );
 }
